3e210a2ec14125e00b160dba1dffc2822c569bd2
Somya Anchalia <sanchalia96@gmail.com>
2024-07-05 11:44:34 +0100

    Importing express module and defining port

---
+++ flight-path-tracker/server.js
@@ -0,0 +1,7 @@
+// importing the express module
+const express = require('express');
+// creating an instance of an Express application
+const app = express();
+// defining port for incoming HTTP requests.
+const port = 8080;
+
