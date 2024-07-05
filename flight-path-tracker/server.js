23adcb52c8143ec4b2679a809246418df65c5f2f
Somya Anchalia <sanchalia96@gmail.com>
2024-07-05 12:45:13 +0100

    implemented the calculate endpoint to handle API request and response

--- flight-path-tracker/server.js
+++ flight-path-tracker/server.js
@@ -1,7 +1,45 @@
-// importing the express module
+// Importing the express module
 const express = require('express');
-// creating an instance of an Express application
+// Creating an instance of an Express application
 const app = express();
-// defining port for incoming HTTP requests.
+// Defining port for incoming HTTP requests.
 const port = 8080;
 
+
+app.use(express.json());
+
+// Defining the function to calculate flight path
+const calculateFlightPath = (flights) => {
+    // Sample hardcoded flight path
+    flightPath = ['DEL', 'LHR', 'JFK']
+    return flightPath;
+};
+
+// Defining the calculate endpoint
+app.post('/calculate', (request, response) => {
+
+    // Parsing request body
+    const all_flights = request.body.all_flights;
+
+    // Validation of user input
+    if (!Array.isArray(all_flights)) {
+        return response.status(400).json({ error: 'Input format is invalid' });
+    }
+
+    // Function call to calculate flight path
+    const flightPath = calculateFlightPath(all_flights);
+
+    // Handling response 
+    if (flightPath) {
+        // Return only the start and end points
+        response.json([flightPath[0], flightPath[flightPath.length-1]]);
+    } else {
+        response.status(400).json({ error: 'Unable to calculate the flight path' });
+    }
+    });
+
+    // Starting the server and listening on the port 8080
+    app.listen(port, () => {
+    console.log(`Flight path tracker listening at http://localhost:${port}`);
+});
+  
