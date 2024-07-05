a972e3beec95a3011c50f7b55631eb69a0e575dd
Somya Anchalia <sanchalia96@gmail.com>
2024-07-05 13:19:17 +0100

    implemented the logic to calculate the actual flight path

--- flight-path-tracker/server.js
+++ flight-path-tracker/server.js
@@ -1,5 +1,5 @@
 // Importing the express module
-const express = require('express');
+const express = require("express");
 // Creating an instance of an Express application
 const app = express();
 // Defining port for incoming HTTP requests.
@@ -10,66 +10,73 @@ app.use(express.json());
 
 // Function to calculate flight path
 const calculateFlightPath = (all_flights) => {
+  // Variable to store flight path
+  const flightsPath = [];
 
-    // Variable to store flight path
-    const flightsPath = ['DEL', 'LHR', 'JFK'];
+  // Return null if there are no flights to process
+  if (!all_flights.length) return null;
 
-    // Return null if there are no flights to process
-    if (!all_flights.length) return null;
+  // Maps to store flight pairs (Source->Destination) and (Destination->Source)
+  const originalFlightMap = new Map();
+  const reverseFlightMap = new Map();
 
-    // Maps to store flight pairs (Source->Destination) and (Destination->Source)
-    const originalFlightMap = new Map();
-    const reverseFlightMap = new Map();
-  
-    // Within the loop, each source-destination pair is added to flightMap, and each destination-source pair is added to reverseMap
-    all_flights.forEach(([source, destination]) => {
-        originalFlightMap.set(source, destination);
-        reverseFlightMap.set(destination, source);
-    });
+  // Within the loop, each source-destination pair is added to flightMap, and each destination-source pair is added to reverseMap
+  all_flights.forEach(([source, destination]) => {
+    originalFlightMap.set(source, destination);
+    reverseFlightMap.set(destination, source);
+  });
 
-    console.log("Original FlightMap :")
-    console.log(originalFlightMap)
-    console.log("Reverse FlightMap : ")
-    console.log(reverseFlightMap)
+  console.log("Original FlightMap :");
+  console.log(originalFlightMap);
+  console.log("Reverse FlightMap : ");
+  console.log(reverseFlightMap);
 
-    // Calculating the starting airport code of the flight path
-    let startingAirport = null;
-    for (const [source] of originalFlightMap) {
-      if (!reverseFlightMap.has(source)) {
-        startingAirport = source;
-        break;
-      }
+  // Calculating the starting airport code of the flight path
+  let startingAirport = null;
+  for (const [source] of originalFlightMap) {
+    if (!reverseFlightMap.has(source)) {
+      startingAirport = source;
+      break;
     }
-    console.log("Starting airport : " + startingAirport)
+  }
+  console.log("Starting airport : " + startingAirport);
 
-    return flightsPath;
+  // Sorting the flights and calculating the flights path
+  let currentAirport = startingAirport;
+  while (currentAirport) {
+    console.log("Current Airport: " + currentAirport);
+    flightsPath.push(currentAirport);
+    currentAirport = originalFlightMap.get(currentAirport);
+  }
+  console.log("Flights Path : ");
+  console.log(flightsPath);
+
+  return flightsPath;
 };
 
 // Defining the calculate endpoint
-app.post('/calculate', (request, response) => {
+app.post("/calculate", (request, response) => {
+  // Parsing request body
+  const all_flights = request.body.all_flights;
 
-    // Parsing request body
-    const all_flights = request.body.all_flights;
+  // Validation of user input
+  if (!Array.isArray(all_flights)) {
+    return response.status(400).json({ error: "Input format is invalid" });
+  }
 
-    // Validation of user input
-    if (!Array.isArray(all_flights)) {
-        return response.status(400).json({ error: 'Input format is invalid' });
-    }
+  // Function call to calculate flight path
+  const flightPath = calculateFlightPath(all_flights);
 
-    // Function call to calculate flight path
-    const flightPath = calculateFlightPath(all_flights);
-
-    // Handling response 
-    if (flightPath) {
-        // Return only the start and end points
-        response.json([flightPath[0], flightPath[flightPath.length-1]]);
-    } else {
-        response.status(400).json({ error: 'Unable to calculate the flight path' });
-    }
-    });
-
-    // Starting the server and listening on the port 8080
-    app.listen(port, () => {
-    console.log(`Flight path tracker listening at http://localhost:${port}`);
+  // Handling response
+  if (flightPath) {
+    // Return only the start and end points
+    response.json([flightPath[0], flightPath[flightPath.length - 1]]);
+  } else {
+    response.status(400).json({ error: "Unable to calculate the flight path" });
+  }
 });
-  
+
+// Starting the server and listening on the port 8080
+app.listen(port, () => {
+  console.log(`Flight path tracker listening at http://localhost:${port}`);
+});
