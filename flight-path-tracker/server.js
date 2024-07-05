4bcda2132ffae974e5f98b5d85f3c61ec061d831
Somya Anchalia <sanchalia96@gmail.com>
2024-07-05 13:52:16 +0100

    logging the data for debugging pusposes

--- flight-path-tracker/server.js
+++ flight-path-tracker/server.js
@@ -8,20 +8,38 @@ const port = 8080;
 // Adding middleware to express app for parsing incoming JSON requests
 app.use(express.json());
 
+// Modules to write logs to local file
+const fs = require('fs');
+const path = require('path');
+
+// File path for storing logs
+const logFilePath = path.join(__dirname, 'logs.log');
+
+// Function to store logs in a separate file
+function logMessage(message) {
+  const timestamp = new Date().toISOString();
+  const logEntry = `${timestamp} - ${message}\n`;
+  fs.appendFile(logFilePath, logEntry, (err) => {
+    if (err) {
+      console.error('Error writing to log file:', err);
+    }
+  });
+}
+
 // Function to calculate flight path
-const calculateFlightPath = (all_flights) => {
+const calculateFlightPath = (allFlights) => {
   // Variable to store flight path
   const flightsPath = [];
 
   // Return null if there are no flights to process
-  if (!all_flights.length) return null;
+  if (!allFlights.length) return null;
 
   // Maps to store flight pairs (Source->Destination) and (Destination->Source)
   const originalFlightMap = new Map();
   const reverseFlightMap = new Map();
 
   // Within the loop, each source-destination pair is added to flightMap, and each destination-source pair is added to reverseMap
-  all_flights.forEach(([source, destination]) => {
+  allFlights.forEach(([source, destination]) => {
     originalFlightMap.set(source, destination);
     reverseFlightMap.set(destination, source);
   });
@@ -34,6 +52,7 @@ const calculateFlightPath = (all_flights) => {
       break;
     }
   }
+  logMessage("Starting airport : " + startingAirport);
 
   // Sorting the flights and calculating the flights path
   let currentAirport = startingAirport;
@@ -42,26 +61,33 @@ const calculateFlightPath = (all_flights) => {
     currentAirport = originalFlightMap.get(currentAirport);
   }
 
+  // Logging the full flight path
+  logMessage("Flights Path : "+ flightsPath.toString());
   return flightsPath;
 };
 
 // Defining the calculate endpoint
 app.post("/calculate", (request, response) => {
   // Parsing request body
-  const all_flights = request.body.all_flights;
+  const allFlights = request.body.all_flights;
 
   // Validation of user input
-  if (!Array.isArray(all_flights)) {
+  if (!Array.isArray(allFlights)) {
     return response.status(400).json({ error: "Input format is invalid" });
   }
 
   // Function call to calculate flight path
-  const flightPath = calculateFlightPath(all_flights);
+  const flightPath = calculateFlightPath(allFlights);
 
   // Handling response
   if (flightPath) {
     // Return only the start and end points
-    response.json([flightPath[0], flightPath[flightPath.length - 1]]);
+    const startEndAirports = [flightPath[0], flightPath[flightPath.length - 1]]
+
+    // Logging the starting and ending airport codes
+    logMessage("Boarding and Destination airport codes: "+ startEndAirports.toString());
+
+    response.json(startEndAirports);
   } else {
     response.status(400).json({ error: "Unable to calculate the flight path" });
   }
@@ -69,5 +95,6 @@ app.post("/calculate", (request, response) => {
 
 // Starting the server and listening on the port 8080
 app.listen(port, () => {
-  console.log(`Flight path tracker listening at http://localhost:${port}`);
+  logMessage(` ============ Flight path tracker listening at http://localhost:${port} =========`);
+  console.log(` ============ Flight path tracker listening at http://localhost:${port} =========`);
 });
